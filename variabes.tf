b120c43879d6adabb02d92577c1a1bd85b2a2b66
Pradyumna-AVD <naikpradyumna295@gmail.com>
2024-06-29 04:17:47 +0530

    1

--- variabes.tf
+++
@@ -1,9 +0,0 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "The AWS access key ID"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "The AWS secret access key"
-  type        = string
-}
