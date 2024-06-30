277bb31fb95636cf5ca54e8e5e1ad724d53cb38b
Pradyumna-AVD <naikpradyumna295@gmail.com>
2024-06-30 18:16:35 +0530

    1

--- ec2.tf
+++ ec2.tf
@@ -24,31 +24,35 @@ resource "aws_instance" "strapi_instance" {
     Name = "Strapi-Instance"
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install docker.io -y
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo usermod -aG docker ubuntu
-              sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              sudo chmod +x /usr/local/bin/docker-compose
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("/mnt/data/Intern.pem")
+      host        = self.public_ip
+    }
 
-              # Create docker-compose.yml
-              cat <<EOT >> /home/ubuntu/docker-compose.yml
-              version: '3'
-              services:
-                strapi:
-                  image: strapi/strapi
-                  ports:
-                    - "1337:1337"
-                  volumes:
-                    - ./app:/srv/app
-              EOT
-
-              # Run Docker Compose
-              sudo docker-compose -f /home/ubuntu/docker-compose.yml up -d
-              EOF
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install docker.io -y",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo usermod -aG docker ubuntu",
+      "sudo curl -L 'https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "cat <<EOT >> /home/ubuntu/docker-compose.yml
+version: '3'
+services:
+  strapi:
+    image: strapi/strapi
+    ports:
+      - '1337:1337'
+    volumes:
+      - ./app:/srv/app
+EOT",
+      "sudo docker-compose -f /home/ubuntu/docker-compose.yml up -d"
+    ]
+  }
 }
 
 output "instance_ip" {
